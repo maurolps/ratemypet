b09804672c4f96ec6920cd35fffd71fda23288b5
maurolps <boroengenharia@gmail.com>
2025-08-18 22:29:09 -0300

    test(controller): ensure CreateUser usecase is called properly

--- backend/src/main/modules/presentation/controllers/create-user.controller.spec.ts
+++ backend/src/main/modules/presentation/controllers/create-user.controller.spec.ts
@@ -1,8 +1,30 @@
+import type { User } from "../../domain/entities/user";
+import type {
+  CreateUser,
+  CreateUserDTO,
+} from "../../domain/usecases/create-user";
 import { CreateUserController } from "./create-user.controller";
+
+class CreateUserStub implements CreateUser {
+  async create(user: CreateUserDTO): Promise<User> {
+    return {
+      id: "valid_id",
+      name: user.name,
+      email: user.email,
+      passwordHash: "hashed_password",
+    };
+  }
+}
+
+const makeSut = () => {
+  const createUserStub = new CreateUserStub();
+  const sut = new CreateUserController(createUserStub);
+  return { sut, createUserStub };
+};
 
 describe("CreateUserController", () => {
   it("Should return 201 when user is created successfully", async () => {
-    const sut = new CreateUserController();
+    const { sut } = makeSut();
     const dummyRequest = {
       body: {
         name: "valid_name",
@@ -11,7 +33,30 @@ describe("CreateUserController", () => {
         confirmPassword: "valid_password",
       },
     };
+
     const httpResponse = await sut.handle(dummyRequest);
+
     expect(httpResponse.status).toBe(201);
+  });
+
+  it("Should call CreateUser usecase with correct values", async () => {
+    const { sut, createUserStub } = makeSut();
+    const createUserSpy = vi.spyOn(createUserStub, "create");
+    const dummyRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        confirmPassword: "any_password",
+      },
+    };
+
+    await sut.handle(dummyRequest);
+
+    expect(createUserSpy).toHaveBeenCalledWith({
+      name: dummyRequest.body.name,
+      email: dummyRequest.body.email,
+      password: dummyRequest.body.password,
+    });
   });
 });
