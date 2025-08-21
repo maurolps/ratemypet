cb026e22fc631b50049f36824c75d8dee89cadd8
maurolps <boroengenharia@gmail.com>
2025-08-21 17:38:12 -0300

    feat(user): ensure CreateUserController return 500 if usecase throws

--- backend/src/main/modules/presentation/controllers/create-user.controller.ts
+++ backend/src/main/modules/presentation/controllers/create-user.controller.ts
@@ -9,30 +9,38 @@ type HttpRequest = {
 };
 
 type HttpResponse = {
-  body: {
+  body?: {
     id: string;
     name: string;
     email: string;
   };
+  error?: unknown;
   status: number;
 };
 
 export class CreateUserController {
   constructor(private readonly createUser: CreateUser) {}
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { name, email, password } = request.body;
-    const createdUser = await this.createUser.execute({
-      name,
-      email,
-      password,
-    });
-    return {
-      body: {
-        id: createdUser.id,
-        name: createdUser.name,
-        email: createdUser.email,
-      },
-      status: 201,
-    };
+    try {
+      const { name, email, password } = request.body;
+      const user = await this.createUser.execute({
+        name,
+        email,
+        password,
+      });
+      return {
+        body: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+        status: 201,
+      };
+    } catch (_) {
+      return {
+        error: new Error("Internal server error"),
+        status: 500,
+      };
+    }
   }
 }
