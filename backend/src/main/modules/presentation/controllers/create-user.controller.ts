aace9328a1dc2860705c548d1337199ab78f796e
maurolps <boroengenharia@gmail.com>
2025-08-28 22:36:06 -0300

    refactor(controller): decouples HttpValidator from controller

--- backend/src/main/modules/presentation/controllers/create-user.controller.ts
+++ backend/src/main/modules/presentation/controllers/create-user.controller.ts
@@ -1,18 +1,19 @@
-import { HttpValidator } from "@presentation/validation/http-validator";
+import type { HttpValidator } from "@presentation/contracts/http-validator.contract";
 import type { CreateUser } from "../../domain/usecases/create-user";
 import type { HttpRequest } from "../dtos/http-request.dto";
 import type { HttpResponse } from "../dtos/http-response.dto";
 import { ErrorPresenter } from "../errors/error-presenter";
 import { created } from "../http/http-helpers";
-import { createUserSchema } from "@presentation/validation/create-user.schema";
 
 export class CreateUserController {
-  constructor(private readonly createUser: CreateUser) {}
+  constructor(
+    private readonly createUser: CreateUser,
+    private readonly httpValidator: HttpValidator,
+  ) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
-      const httpValidator = new HttpValidator(createUserSchema);
-      const userDTO = httpValidator.execute(request);
+      const userDTO = this.httpValidator.execute(request);
 
       const { name, email, password } = userDTO;
       const user = await this.createUser.execute({
--- backend/src/main/modules/presentation/validation/zod-http-validator.ts
+++ backend/src/main/modules/presentation/validation/zod-http-validator.ts
@@ -1,10 +1,11 @@
 import { AppError } from "@presentation/errors/app-error";
 import type { CreateUserDTO } from "@domain/usecases/create-user";
 import type { HttpRequest } from "@presentation/dtos/http-request.dto";
-import type { createUserSchema } from "../validation/create-user.schema";
+import type { createUserSchema } from "./create-user.schema";
+import type { HttpValidator } from "@presentation/contracts/http-validator.contract";
 import type { z } from "zod";
 
-export class HttpValidator {
+export class ZodHttpValidator implements HttpValidator {
   constructor(private readonly schema: z.ZodType) {}
   execute(request: HttpRequest): CreateUserDTO {
     type Schema = z.infer<typeof createUserSchema>;
