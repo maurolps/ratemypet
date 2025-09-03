78ee754183361ca50e686ed9ac700316e82d52fa
maurolps <boroengenharia@gmail.com>
2025-09-02 21:48:06 -0300

    test(validation): ensure ZodHttpValidation throws if a parameter is missing

--- backend/tests/presentation/validation/zod-http-validator.spec.ts
+++ backend/tests/presentation/validation/zod-http-validator.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { ZodHttpValidator } from "@presentation/validation/zod-http-validator";
 import { createUserSchema } from "@presentation/validation/create-user.schema";
 import { AppError } from "@presentation/errors/app-error";
+import type { CreateUserDTO } from "@domain/usecases/create-user";
 
 describe("ZodHttpValidator", () => {
   it("Should return a UserDTO when validating a valid request body", () => {
@@ -29,4 +30,21 @@ describe("ZodHttpValidator", () => {
       new AppError("MISSING_BODY"),
     );
   });
+
+  it.each([["name"], ["email"], ["password"]])(
+    "Should throw an AppError if %s is missing",
+    (missingParam) => {
+      const sut = new ZodHttpValidator(createUserSchema);
+      const request: CreateUserDTO = {
+        name: "valid_name",
+        email: "valid_email@mail.com",
+        password: "valid_password",
+      };
+      delete request[missingParam as keyof typeof request];
+
+      expect(() => sut.execute({ body: request })).toThrowError(
+        new AppError("MISSING_PARAM", missingParam),
+      );
+    },
+  );
 });
