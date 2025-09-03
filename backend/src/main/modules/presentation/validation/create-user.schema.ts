5df2699b00f0fc9d13cb7aeb8936ea4813e71530
maurolps <boroengenharia@gmail.com>
2025-09-02 22:03:23 -0300

    feat(validation): ensure ZodHttpValidation throws if email is invalid

--- backend/src/main/modules/presentation/validation/create-user.schema.ts
+++ backend/src/main/modules/presentation/validation/create-user.schema.ts
@@ -4,7 +4,7 @@ export const createUserSchema = z.object({
   body: z.object(
     {
       name: z.string("MISSING_PARAM"),
-      email: z.string("MISSING_PARAM").email(),
+      email: z.string("MISSING_PARAM").email("INVALID_PARAM"),
       password: z.string("MISSING_PARAM"),
     },
     { error: "MISSING_BODY" },
