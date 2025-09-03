c4ba898dcf12598a9264e8ee9c43c912ee37d87c
maurolps <boroengenharia@gmail.com>
2025-09-02 22:23:54 -0300

    feat(validation): add minimum length validation for name in createUserSchema

--- backend/src/main/modules/presentation/validation/create-user.schema.ts
+++ backend/src/main/modules/presentation/validation/create-user.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createUserSchema = z.object({
   body: z.object(
     {
-      name: z.string("MISSING_PARAM"),
+      name: z.string("MISSING_PARAM").min(3, "INVALID_NAME"),
       email: z.string("MISSING_PARAM").email("INVALID_PARAM"),
       password: z.string("MISSING_PARAM").min(6, "WEAK_PASSWORD"),
     },
