8e161b8c9a29803ec038e90dc1cd309457e8f3fb
maurolps <boroengenharia@gmail.com>
2025-09-02 22:11:33 -0300

    feat(validation): ensure ZodHttpValidation throws if password is invalid

--- backend/src/main/modules/presentation/validation/create-user.schema.ts
+++ backend/src/main/modules/presentation/validation/create-user.schema.ts
@@ -5,7 +5,7 @@ export const createUserSchema = z.object({
     {
       name: z.string("MISSING_PARAM"),
       email: z.string("MISSING_PARAM").email("INVALID_PARAM"),
-      password: z.string("MISSING_PARAM"),
+      password: z.string("MISSING_PARAM").min(6, "WEAK_PASSWORD"),
     },
     { error: "MISSING_BODY" },
   ),
