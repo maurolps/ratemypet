bd98ba9189e866c52f14c71b6193891effe76129
maurolps <boroengenharia@gmail.com>
2025-09-23 19:27:03 -0300

    refactor(tests): replace 'postgres' library with 'pg' in UserRepository tests

--- backend/tests/infra/db/postgres/user.repository.test.ts
+++ backend/tests/infra/db/postgres/user.repository.test.ts
@@ -4,26 +4,31 @@ import {
   PostgreSqlContainer,
   type StartedPostgreSqlContainer,
 } from "@testcontainers/postgresql";
-import postgres from "postgres";
+import { Pool, type PoolClient } from "pg";
 import { dbmateMigrate } from "@infra/db/postgres/helpers/dbmate-migrate";
 
 describe("Postgres UserRepository", () => {
   let pgContainer: PostgreSqlContainer;
   let pgConnection: StartedPostgreSqlContainer;
-  let sql: postgres.Sql;
+  let pgClient: PoolClient;
+  let pgPool: Pool;
 
   beforeAll(async () => {
     pgContainer = new PostgreSqlContainer("postgres:17");
     pgConnection = await pgContainer.start();
     const pgUri = pgConnection.getConnectionUri();
     await dbmateMigrate(pgUri);
-    sql = postgres(pgUri);
+    pgPool = new Pool({
+      connectionString: pgUri,
+    });
+    pgClient = await pgPool.connect();
   }, 60_000);
 
   afterAll(async () => {
     if (pgConnection) {
+      pgClient.release();
+      await pgPool.end();
       await pgConnection.stop();
-      sql?.end();
     }
   }, 60_000);
 
@@ -35,12 +40,15 @@ describe("Postgres UserRepository", () => {
     };
     const { name, email, passwordHash } = userDTO;
 
-    const user: User[] = await sql`
-      INSERT INTO users ( name, email, password_hash)
-      VALUES (${name}, ${email}, ${passwordHash})
-      RETURNING id, name, email    
-    `;
+    const user = await pgClient.query<User>(
+      `
+          INSERT INTO users (name, email, password_hash)
+          VALUES ($1, $2, $3)
+          RETURNING id, name, email
+        `,
+      [name, email, passwordHash],
+    );
 
-    expect(user[0].name).toEqual("valid_name");
+    expect(user.rows[0].name).toEqual("valid_name");
   });
 });
