aa90c975ec7409265cfd2718e6df8fd6c2bc677b
maurolps <boroengenharia@gmail.com>
2025-09-25 16:24:23 -0300

    feat(db): implement PgUserRepository logic for user creation and move migration helper to tests

---
+++ backend/src/main/modules/infra/db/postgres/pg-user.repository.ts
@@ -0,0 +1,20 @@
+import type { CreateUserRepository } from "@application/repositories/create-user.repository";
+import type { User } from "@domain/entities/user";
+import type { CreateUserDTO } from "@domain/usecases/create-user.contract";
+import type { PgPool } from "./helpers/pg-pool";
+import { CREATE_USER } from "./sql/user.sql";
+
+export class PgUserRepository implements CreateUserRepository {
+  constructor(private readonly pool: PgPool) {}
+
+  async perform(userDTO: CreateUserDTO): Promise<User> {
+    const { name, email, password: passwordHash } = userDTO;
+    const userRows = await this.pool.query<User>(CREATE_USER, [
+      name,
+      email,
+      passwordHash,
+    ]);
+    const user = userRows.rows[0];
+    return user;
+  }
+}
