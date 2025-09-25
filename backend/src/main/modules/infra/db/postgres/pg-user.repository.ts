2318934eb855cbf1b9496f4ed14fc4149096b78e
maurolps <boroengenharia@gmail.com>
2025-09-25 19:54:44 -0300

    feat(users): add findByEmail method and corresponding SQL query in PgUserRepository

--- backend/src/main/modules/infra/db/postgres/pg-user.repository.ts
+++ backend/src/main/modules/infra/db/postgres/pg-user.repository.ts
@@ -1,10 +1,13 @@
 import type { CreateUserRepository } from "@application/repositories/create-user.repository";
+import type { FindUserByEmailRepository } from "@application/repositories/find-user-by-email.repository";
 import type { User } from "@domain/entities/user";
 import type { CreateUserDTO } from "@domain/usecases/create-user.contract";
 import type { PgPool } from "./helpers/pg-pool";
-import { CREATE_USER } from "./sql/user.sql";
+import { CREATE_USER, FIND_BY_EMAIL } from "./sql/user.sql";
 
-export class PgUserRepository implements CreateUserRepository {
+export class PgUserRepository
+  implements CreateUserRepository, FindUserByEmailRepository
+{
   constructor(private readonly pool: PgPool) {}
 
   async create(userDTO: CreateUserDTO): Promise<User> {
@@ -14,6 +17,12 @@ export class PgUserRepository implements CreateUserRepository {
       email,
       passwordHash,
     ]);
+    const user = userRows.rows[0];
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const userRows = await this.pool.query<User>(FIND_BY_EMAIL, [email]);
     const user = userRows.rows[0];
     return user;
   }
--- backend/src/main/modules/infra/db/postgres/sql/user.sql.ts
+++ backend/src/main/modules/infra/db/postgres/sql/user.sql.ts
@@ -3,3 +3,9 @@ export const CREATE_USER = `
   VALUES ($1, $2, $3)
   RETURNING id, name, email
 `;
+
+export const FIND_BY_EMAIL = `
+  SELECT id, name, email
+  FROM users
+  WHERE email = $1
+  `;
