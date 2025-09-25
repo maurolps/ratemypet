847b894dbd6be5018d95da2189bed2463c5b428e
maurolps <boroengenharia@gmail.com>
2025-09-25 18:35:53 -0300

    refactor(tests): mock bcrypt with spy to improve tests performance

--- backend/tests/infra/security/bcrypt.adapter.spec.ts
+++ backend/tests/infra/security/bcrypt.adapter.spec.ts
@@ -5,15 +5,16 @@ import { vi, it, describe, expect } from "vitest";
 describe("Bcrypt Adapter", () => {
   const SALT_ROUNDS = 12;
   const sut = new BcryptAdapter();
+  const bcryptSpy = vi.spyOn(bcrypt, "hash");
 
   it("Should call bcrypt with correct salt value", async () => {
-    const hashSpy = vi.spyOn(bcrypt, "hash");
+    const hashSpy = bcryptSpy.mockImplementationOnce(() => "hashed_password");
     await sut.execute("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", SALT_ROUNDS);
   });
 
   it("Should rethrow if bcrypt throws", async () => {
-    vi.spyOn(bcrypt, "hash").mockImplementationOnce(() => {
+    bcryptSpy.mockImplementationOnce(() => {
       throw new Error();
     });
     const promise = sut.execute("any_value");
