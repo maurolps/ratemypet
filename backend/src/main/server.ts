9ade4137cb459a1944bf273ac55330d5cceca8ed
maurolps <boroengenharia@gmail.com>
2025-10-08 19:57:17 -0300

    refactor(env): centralize environment default config

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -1,19 +1,17 @@
 import { PgPool } from "@infra/db/postgres/helpers/pg-pool";
 import { makeApp } from "./http/app";
+import { env } from "./config/env";
 
 const db = PgPool.getInstance();
 
 const bootstrap = async () => {
-  const PORT = process.env.PORT || 8000;
-  const DATABASE_URL = process.env.DATABASE_URL || "";
-
-  db.connect(DATABASE_URL);
+  db.connect(env.DATABASE_URL);
   const dbReady = await db.health();
 
   if (dbReady) {
     const app = makeApp();
-    app.listen(PORT, () => {
-      console.log("Server listening on port: ", PORT);
+    app.listen(env.PORT, () => {
+      console.log("Server listening on port: ", env.PORT);
     });
   }
 };
