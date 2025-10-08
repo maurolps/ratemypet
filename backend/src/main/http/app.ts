b10529b9f90ee5ada7d992eff96f2fa612fcb7bb
maurolps <boroengenharia@gmail.com>
2025-10-07 23:34:36 -0300

    feat(main): add global error handling middleware

--- backend/src/main/http/app.ts
+++ backend/src/main/http/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { setupGlobalMiddlewares } from "./middlewares/global";
 import { setupRoutes } from "./routes/setup";
+import { setupGlobalMiddlewares } from "./middlewares/global";
+import { errorHandler } from "./middlewares/error-handler";
 
 export const makeApp = () => {
   const app = express();
   setupGlobalMiddlewares(app);
   setupRoutes(app);
+  app.use(errorHandler);
   return app;
 };
