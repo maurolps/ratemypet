1a062eb0256857b5b710ef2b63611306bac89174
maurolps <boroengenharia@gmail.com>
2025-11-01 17:45:49 -0300

    refactor(tests): ensure LoginValidatorStub implements HttpValidator

--- backend/tests/presentation/doubles/login.validator.stub.ts
+++ backend/tests/presentation/doubles/login.validator.stub.ts
@@ -1,11 +1,8 @@
 import type { HttpRequest } from "@presentation/dtos/http-request.dto";
+import type { LoginDTO } from "@domain/usecases/login.contract";
+import type { HttpValidator } from "@presentation/contracts/http-validator.contract";
 
-interface LoginDTO {
-  email: string;
-  password: string;
-}
-
-export class LoginValidatorStub {
+export class LoginValidatorStub implements HttpValidator<LoginDTO> {
   execute(request: HttpRequest): LoginDTO {
     request.body = {
       email: "valid_email@mail.com",
