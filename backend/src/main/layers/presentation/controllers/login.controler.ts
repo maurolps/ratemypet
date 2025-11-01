dd0606aa8eb4477ffd7c4beb18f1fce348a9dae4
maurolps <boroengenharia@gmail.com>
2025-10-31 21:26:30 -0300

    feat(login):  add error handler and validator to the LoginController

--- backend/src/main/layers/presentation/controllers/login.controler.ts
+++ backend/src/main/layers/presentation/controllers/login.controler.ts
@@ -1,10 +1,21 @@
 import type { Controller } from "@presentation/contracts/controller.contract";
 import type { HttpRequest } from "@presentation/dtos/http-request.dto";
 import type { HttpResponse } from "@presentation/dtos/http-response.dto";
+import { ErrorPresenter } from "@presentation/errors/error-presenter";
 import { ok } from "@presentation/http/http-helpers";
 
+interface LoginHttpValidator {
+  execute(request: HttpRequest): { email: string; password: string };
+}
+
 export class LoginController implements Controller {
+  constructor(private readonly httpValidator: LoginHttpValidator) {}
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    return ok(`Login successful: ${request.body}`);
+    try {
+      const loginDTO = this.httpValidator.execute(request);
+      return ok(`Login successful: ${loginDTO}`);
+    } catch (error) {
+      return ErrorPresenter(error);
+    }
   }
 }
