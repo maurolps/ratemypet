547b9b1f4275996f51bb671621f4665257a6f751
maurolps <boroengenharia@gmail.com>
2025-11-01 17:17:35 -0300

    feat(validation): add validation schema and FakeDataDTO interface

---
+++ backend/tests/presentation/mocks/validation-schema.mock.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const validationSchema = z.object({
+  body: z.object(
+    {
+      name: z
+        .string("MISSING_PARAM")
+        .min(3, "INVALID_NAME")
+        .max(70, "INVALID_PARAM")
+        .trim()
+        .normalize("NFKC")
+        .transform((val) => val.replace(/\s+/g, " ")),
+      email: z
+        .string("MISSING_PARAM")
+        .trim()
+        .email("INVALID_PARAM")
+        .toLowerCase(),
+      password: z
+        .string("MISSING_PARAM")
+        .min(6, "WEAK_PASSWORD")
+        .max(32, "INVALID_PARAM"),
+    },
+    { error: "MISSING_BODY" },
+  ),
+});
+
+export interface FakeDataDTO {
+  email: string;
+  name: string;
+  password: string;
+}
