d265382650fcc2547110b3b5c63ec1e3d213a1e3
maurolps <boroengenharia@gmail.com>
2025-11-03 18:27:52 -0300

    feat(login): add LoginUseCase and AuthData contracts #48

--- backend/src/main/layers/domain/usecases/login.contract.ts
+++ backend/src/main/layers/domain/usecases/login.contract.ts
@@ -2,3 +2,12 @@ export interface LoginDTO {
   email: string;
   password: string;
 }
+
+export type AuthData = {
+  accessToken: string;
+  refreshToken: string;
+};
+
+export interface Login {
+  auth(loginDTO: LoginDTO): Promise<AuthData>;
+}
