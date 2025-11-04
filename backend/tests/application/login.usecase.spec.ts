35bc6661c0b84c45e722ecc3fdd666e2081d37cb
maurolps <boroengenharia@gmail.com>
2025-11-04 18:04:25 -0300

    refactor(tests): simplify setup in LoginUseCase tests by extracting makeSut function

--- backend/tests/application/login.usecase.spec.ts
+++ backend/tests/application/login.usecase.spec.ts
@@ -5,33 +5,37 @@ import { AppError } from "@application/errors/app-error";
 import { HasherStub } from "./doubles/hasher.stub";
 
 describe("LoginUseCase", () => {
+  const makeSut = () => {
+    const hasherStub = new HasherStub();
+    const findUserByEmailStub = new FindUserByEmailRepositoryStub();
+    const findUserByEmailSpy = vi.spyOn(findUserByEmailStub, "findByEmail");
+    const sut = new LoginUseCase(findUserByEmailStub, hasherStub);
+    return { sut, findUserByEmailStub, hasherStub, findUserByEmailSpy };
+  };
+
   it("Should throw UNAUTHORIZED error when user is not found", async () => {
+    const { sut } = makeSut();
     const loginDTO = {
       email: "non_exists@mail.com",
       password: "any_password",
     };
-    const hasherStub = new HasherStub();
-    const findUserByEmail = new FindUserByEmailRepositoryStub();
-    const sut = new LoginUseCase(findUserByEmail, hasherStub);
     const promise = sut.auth(loginDTO);
     await expect(promise).rejects.toThrow(new AppError("UNAUTHORIZED"));
   });
 
   it("Should throw UNAUTHORIZED error when password is incorrect", async () => {
+    const { sut, findUserByEmailSpy } = makeSut();
     const loginDTO = {
       email: "valid_email@mail.com",
       password: "wrong_password",
     };
-    const hasherStub = new HasherStub();
-    const findUserByEmail = new FindUserByEmailRepositoryStub();
-    vi.spyOn(findUserByEmail, "findByEmail").mockResolvedValueOnce({
+    findUserByEmailSpy.mockResolvedValueOnce({
       id: "valid_id",
       name: "valid_name",
       email: "valid_email@mail.com",
       created_at: new Date(),
       password_hash: "hashed_valid_password",
     });
-    const sut = new LoginUseCase(findUserByEmail, hasherStub);
     const promise = sut.auth(loginDTO);
     await expect(promise).rejects.toThrow(new AppError("UNAUTHORIZED"));
   });
