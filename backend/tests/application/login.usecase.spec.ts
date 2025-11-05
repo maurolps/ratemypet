23b0e46306895daa7dd9c7d0e3fc6ca2937d3971
maurolps <boroengenharia@gmail.com>
2025-11-05 17:30:49 -0300

    test(login): ensure LoginUseCase calls TokenGenerator with correct values

--- backend/tests/application/login.usecase.spec.ts
+++ backend/tests/application/login.usecase.spec.ts
@@ -1,15 +1,23 @@
 import { LoginUseCase } from "@application/usecases/login.usecase";
 import { FindUserByEmailRepositoryStub } from "./doubles/find-user-by-email.repository.stub";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { AppError } from "@application/errors/app-error";
 import { HasherStub } from "./doubles/hasher.stub";
+import { TokenGeneratorStub } from "./doubles/token-generator.stub";
+import e from "express";
 
 describe("LoginUseCase", () => {
   const makeSut = () => {
     const hasherStub = new HasherStub();
+    const tokenGeneratorStub = new TokenGeneratorStub();
+    const tokenGeneratorSpy = vi.spyOn(tokenGeneratorStub, "issue");
     const findUserByEmailStub = new FindUserByEmailRepositoryStub();
-    const sut = new LoginUseCase(findUserByEmailStub, hasherStub);
-    return { sut };
+    const sut = new LoginUseCase(
+      findUserByEmailStub,
+      hasherStub,
+      tokenGeneratorStub,
+    );
+    return { sut, tokenGeneratorSpy };
   };
 
   it("Should throw UNAUTHORIZED error when user is not found", async () => {
@@ -30,5 +38,19 @@ describe("LoginUseCase", () => {
     };
     const promise = sut.auth(loginDTO);
     await expect(promise).rejects.toThrow(new AppError("UNAUTHORIZED"));
+  });
+
+  it("Should call TokenGenerator with correct values", async () => {
+    const { sut, tokenGeneratorSpy } = makeSut();
+    const loginDTO = {
+      email: "valid_email@mail.com",
+      password: "valid_password",
+    };
+    await sut.auth(loginDTO);
+    expect(tokenGeneratorSpy).toHaveBeenCalledWith({
+      sub: "valid_id",
+      name: "valid_name",
+      email: "valid_email@mail.com",
+    });
   });
 });
