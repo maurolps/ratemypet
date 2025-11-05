dd8938b6965c24bb9d8d36d008b7c95bcf23ee59
maurolps <boroengenharia@gmail.com>
2025-11-05 17:04:38 -0300

    refactor(tests): remove unnecessary spy from LoginUseCase tests

--- backend/tests/application/login.usecase.spec.ts
+++ backend/tests/application/login.usecase.spec.ts
@@ -1,6 +1,6 @@
 import { LoginUseCase } from "@application/usecases/login.usecase";
 import { FindUserByEmailRepositoryStub } from "./doubles/find-user-by-email.repository.stub";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { AppError } from "@application/errors/app-error";
 import { HasherStub } from "./doubles/hasher.stub";
 
@@ -8,9 +8,8 @@ describe("LoginUseCase", () => {
   const makeSut = () => {
     const hasherStub = new HasherStub();
     const findUserByEmailStub = new FindUserByEmailRepositoryStub();
-    const findUserByEmailSpy = vi.spyOn(findUserByEmailStub, "findByEmail");
     const sut = new LoginUseCase(findUserByEmailStub, hasherStub);
-    return { sut, findUserByEmailStub, hasherStub, findUserByEmailSpy };
+    return { sut };
   };
 
   it("Should throw UNAUTHORIZED error when user is not found", async () => {
@@ -24,18 +23,11 @@ describe("LoginUseCase", () => {
   });
 
   it("Should throw UNAUTHORIZED error when password is incorrect", async () => {
-    const { sut, findUserByEmailSpy } = makeSut();
+    const { sut } = makeSut();
     const loginDTO = {
       email: "valid_email@mail.com",
       password: "wrong_password",
     };
-    findUserByEmailSpy.mockResolvedValueOnce({
-      id: "valid_id",
-      name: "valid_name",
-      email: "valid_email@mail.com",
-      created_at: new Date(),
-      password_hash: "hashed_valid_password",
-    });
     const promise = sut.auth(loginDTO);
     await expect(promise).rejects.toThrow(new AppError("UNAUTHORIZED"));
   });
