44582709905b106104b0f61759f303958e181081
maurolps <boroengenharia@gmail.com>
2025-11-06 19:50:20 -0300

    refactor(config): ensure JWT_ACCESS_TOKEN_SECRET is set from environment variable

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -3,4 +3,6 @@ export const env = {
   DATABASE_URL:
     process.env.DATABASE_URL ||
     "postgres://ratemypet:ratemypet@localhost:5432/ratemypet_dev?sslmode=disable",
+  JWT_ACCESS_TOKEN_SECRET:
+    process.env.JWT_ACCESS_TOKEN_SECRET || "dev_access_token_secret",
 };
