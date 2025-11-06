3c14642b622574d2eebbf3bb5b87018f18b03f36
maurolps <boroengenharia@gmail.com>
2025-11-06 19:51:08 -0300

    refactor(auth): use env variable for JWT_ACCESS_TOKEN_SECRET in JwtAdapter

--- backend/src/main/layers/infra/auth/jwt.adapter.ts
+++ backend/src/main/layers/infra/auth/jwt.adapter.ts
@@ -1,13 +1,11 @@
 import type { TokenGenerator } from "@application/ports/token-generator.contract";
 import type { AccessTokenPayload } from "@domain/entities/token";
+import { env } from "@main/config/env";
 import jwt from "jsonwebtoken";
 
 export class JwtAdapter implements TokenGenerator<AccessTokenPayload> {
   async issue(payload: AccessTokenPayload): Promise<string> {
-    const accessToken = jwt.sign(
-      payload,
-      process.env.JWT_ACCESS_TOKEN_SECRET || "default_secret",
-    );
-    return `access token: ${accessToken}`;
+    const accessToken = jwt.sign(payload, env.JWT_ACCESS_TOKEN_SECRET);
+    return `${accessToken}`;
   }
 }
