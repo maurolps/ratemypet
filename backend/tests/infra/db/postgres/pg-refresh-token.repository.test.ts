d019a07490a7c2e0a0c42a2c51862393a48a2091
maurolps <boroengenharia@gmail.com>
2025-11-14 15:17:48 -0300

    refactor(tests): add beforeAll setup to PgRefreshTokenRepository tests

--- backend/tests/infra/db/postgres/pg-refresh-token.repository.test.ts
+++ backend/tests/infra/db/postgres/pg-refresh-token.repository.test.ts
@@ -1,17 +1,27 @@
-import { it, describe, expect } from "vitest";
+import type { RefreshTokenDTO } from "@domain/entities/token";
+import type { User } from "@domain/entities/user";
+import { it, describe, expect, beforeAll } from "vitest";
 import { PgRefreshTokenRepository } from "@infra/db/postgres/pg-refresh-token.repository";
 import { insertFakeUser } from "./helpers/fake-user";
 
 describe("PgRefreshTokenRepository", () => {
-  it("Should save refresh token without throwing an error", async () => {
-    const sut = new PgRefreshTokenRepository();
-    const user = await insertFakeUser();
-    const refreshTokenDTO = {
+  let refreshTokenDTO: RefreshTokenDTO;
+  let user: User;
+
+  beforeAll(async () => {
+    user = await insertFakeUser();
+    refreshTokenDTO = {
       id: "valid_refresh_token_id",
       user_id: user.id,
       token_hash: "valid_token_hash",
     };
+  });
+
+  it("Should save refresh token without throwing an error", async () => {
+    const sut = new PgRefreshTokenRepository();
     const promise = sut.save(refreshTokenDTO);
     await expect(promise).resolves.not.toThrow();
   });
+
+  it;
 });
