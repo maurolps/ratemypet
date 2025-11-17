d9630fc039a5ae647576280eeb22d726456d2ead
maurolps <boroengenharia@gmail.com>
2025-11-17 17:39:32 -0300

    feat(users): add login route to userRoutes

--- backend/src/main/http/routes/user.routes.ts
+++ backend/src/main/http/routes/user.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { expressAdapter } from "../adapters/express-controller.adapter";
 import { makeCreateUserController } from "@main/composition/users/create-user.controller.factory";
+import { makeLoginController } from "@main/composition/users/login.controller.factory";
 
 export const userRoutes = Router();
 
 userRoutes.post("/users", expressAdapter(makeCreateUserController()));
+userRoutes.post("/users/login", expressAdapter(makeLoginController()));
