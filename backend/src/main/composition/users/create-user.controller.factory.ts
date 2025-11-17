0747c2135109647c03eff26095a807a7645364e2
maurolps <boroengenharia@gmail.com>
2025-11-17 17:00:43 -0300

    feat(users): include token issuer in CreateUserController factory

--- backend/src/main/composition/users/create-user.controller.factory.ts
+++ backend/src/main/composition/users/create-user.controller.factory.ts
@@ -3,10 +3,12 @@ import { CreateUserController } from "@presentation/controllers/create-user.cont
 import { ZodHttpValidator } from "@presentation/validation/zod-http-validator";
 import { createUserSchema } from "@presentation/validation/create-user.schema";
 import { makeCreateUserUseCase } from "./create-user.usecase.factory";
+import { makeTokenIssuer } from "./token-issuer.factory";
 
 export const makeCreateUserController = () => {
   const validator = new ZodHttpValidator<CreateUserDTO>(createUserSchema);
   const usecase = makeCreateUserUseCase();
-  const controller = new CreateUserController(usecase, validator);
+  const tokenIssuer = makeTokenIssuer();
+  const controller = new CreateUserController(usecase, validator, tokenIssuer);
   return controller;
 };
