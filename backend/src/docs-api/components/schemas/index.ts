7a038a347e2487a047f0e9a37aa8049985cede22
maurolps <boroengenharia@gmail.com>
2025-11-17 23:07:43 -0300

    feat(swagger): update Swagger documentation with login route #49

--- backend/src/docs-api/components/schemas/index.ts
+++ backend/src/docs-api/components/schemas/index.ts
@@ -1,10 +1,14 @@
 import userResponse from "./user-response-schema.api";
 import userRequest from "./user-request-schema.api";
 import errorSchema from "./error-schema.api";
+import loginResponse from "./login-response-schema.api";
+import loginRequest from "./login-request-schema.api";
 
 const schemas = {
   userResponse,
   userRequest,
+  loginResponse,
+  loginRequest,
   errorSchema,
 } as const;
 
--- backend/src/docs-api/paths/index.ts
+++ backend/src/docs-api/paths/index.ts
@@ -1,7 +1,9 @@
 import usersCreate from "./create-user-path.api";
+import loginPath from "./login-path.api";
 
 const paths = {
   ...usersCreate,
+  ...loginPath,
 } as const;
 
 export default paths;
