5c80a29e65d14c41bbc75169e76378384210a977
maurolps <boroengenharia@gmail.com>
2025-11-20 20:24:36 -0300

    feat(http): add rate limiter to Login and CreateUser #44

--- backend/src/main/http/routes/user.routes.ts
+++ backend/src/main/http/routes/user.routes.ts
@@ -2,8 +2,20 @@ import { Router } from "express";
 import { expressAdapter } from "../adapters/express-controller.adapter";
 import { makeCreateUserController } from "@main/composition/users/create-user.controller.factory";
 import { makeLoginController } from "@main/composition/users/login.controller.factory";
+import { makeRateLimiter } from "../middlewares/rate-limit";
 
 export const userRoutes = Router();
 
-userRoutes.post("/users", expressAdapter(makeCreateUserController()));
-userRoutes.post("/users/login", expressAdapter(makeLoginController()));
+const loginRateLimit = makeRateLimiter({ limit: 10 });
+const createUserRateLimit = makeRateLimiter({ limit: 5 });
+
+userRoutes.post(
+  "/users",
+  createUserRateLimit,
+  expressAdapter(makeCreateUserController()),
+);
+userRoutes.post(
+  "/users/login",
+  loginRateLimit,
+  expressAdapter(makeLoginController()),
+);
