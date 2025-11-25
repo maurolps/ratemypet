ab456aeb39af146c45f6b48925fe1f652a97864f
maurolps <boroengenharia@gmail.com>
2025-11-24 21:41:51 -0300

    test(users): ensure RefreshTokenController returns MISSING_PARAM if token is missing

--- backend/tests/presentation/refresh-token-controller.spec.ts
+++ backend/tests/presentation/refresh-token-controller.spec.ts
@@ -1,6 +1,7 @@
 import { RefreshTokenController } from "@presentation/controllers/refresh-token.controller";
 import { RefreshTokenValidatorStub } from "./doubles/refresh-token.validator.stub";
 import { describe, expect, it, vi } from "vitest";
+import { AppError } from "@application/errors/app-error";
 
 describe("RefreshTokenController", () => {
   const makeSut = () => {
@@ -30,5 +31,18 @@ describe("RefreshTokenController", () => {
     };
     await sut.handle(dummyRequest);
     expect(refreshTokenValidatorSpy).toHaveBeenCalledWith(dummyRequest);
+  });
+
+  it("Should return 400 if refresh token is missing", async () => {
+    const { sut, refreshTokenValidatorSpy } = makeSut();
+    const dummyRequest = {
+      cookies: {},
+    };
+    refreshTokenValidatorSpy.mockImplementationOnce(() => {
+      throw new AppError("MISSING_PARAM", "refresh token");
+    });
+    const httpResponse = await sut.handle(dummyRequest);
+    expect(httpResponse.status).toBe(400);
+    expect(httpResponse.body.message).toEqual("Missing Param: refresh token");
   });
 });
