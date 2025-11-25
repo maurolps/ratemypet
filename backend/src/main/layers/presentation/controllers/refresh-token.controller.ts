a0f87247972367a68c3d0af589e7a90fff5e9b8a
maurolps <boroengenharia@gmail.com>
2025-11-24 21:41:23 -0300

    feat(users): add error handling in RefreshTokenController

--- backend/src/main/layers/presentation/controllers/refresh-token.controller.ts
+++ backend/src/main/layers/presentation/controllers/refresh-token.controller.ts
@@ -3,13 +3,18 @@ import type { Controller } from "@presentation/contracts/controller.contract";
 import type { HttpValidator } from "@presentation/contracts/http-validator.contract";
 import type { HttpRequest } from "@presentation/dtos/http-request.dto";
 import type { HttpResponse } from "@presentation/dtos/http-response.dto";
+import { ErrorPresenter } from "@presentation/errors/error-presenter";
 
 export class RefreshTokenController implements Controller {
   constructor(
     private readonly httpValidator: HttpValidator<RefreshTokenParsed>,
   ) {}
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const _parsedToken = this.httpValidator.execute(request);
-    return { status: 200, body: {} };
+    try {
+      const _parsedToken = this.httpValidator.execute(request);
+      return { status: 200, body: {} };
+    } catch (error) {
+      return ErrorPresenter(error);
+    }
   }
 }
