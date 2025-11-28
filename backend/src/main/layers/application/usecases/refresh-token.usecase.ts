e91bab996e66d6b01ab80fe3c5787f580241c7c6
maurolps <boroengenharia@gmail.com>
2025-11-27 21:21:34 -0300

    feat(auth): implement findUser in RefreshTokenUseCase

--- backend/src/main/layers/application/usecases/refresh-token.usecase.ts
+++ backend/src/main/layers/application/usecases/refresh-token.usecase.ts
@@ -1,3 +1,5 @@
+import { AppError } from "@application/errors/app-error";
+import type { FindUserRepository } from "@application/repositories/find-user.repository";
 import type { TokenIssuer, Tokens } from "@domain/entities/token";
 import type {
   RefreshToken,
@@ -5,9 +7,16 @@ import type {
 } from "@domain/usecases/refresh-token.contract";
 
 export class RefreshTokenUseCase implements RefreshToken {
-  constructor(private readonly tokenIssuer: TokenIssuer) {}
+  constructor(
+    private readonly tokenIssuer: TokenIssuer,
+    private readonly findUser: FindUserRepository,
+  ) {}
   async execute(token: RefreshTokenParsed): Promise<Tokens> {
-    const _refreshTokenDTO = await this.tokenIssuer.validateRefreshToken(token);
+    const refreshTokenDTO = await this.tokenIssuer.validateRefreshToken(token);
+    const user = await this.findUser.findById(refreshTokenDTO.user_id);
+    if (!user) {
+      throw new AppError("UNAUTHORIZED");
+    }
 
     return {
       accessToken: "newAccessToken",
