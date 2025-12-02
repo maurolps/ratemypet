8df313859e7b1ac6fe7914edc2edeab0154f3899
maurolps <boroengenharia@gmail.com>
2025-12-02 17:11:30 -0300

    feat(http): add refresh token route with rate limiting

--- backend/src/main/http/routes/user.routes.ts
+++ backend/src/main/http/routes/user.routes.ts
@@ -3,11 +3,13 @@ import { expressAdapter } from "../adapters/express-controller.adapter";
 import { makeCreateUserController } from "@main/composition/users/create-user.controller.factory";
 import { makeLoginController } from "@main/composition/users/login.controller.factory";
 import { makeRateLimiter } from "../middlewares/rate-limit";
+import { makeRefreshTokenController } from "@main/composition/users/refresh-token.controller.factory";
 
 export const userRoutes = Router();
 
 const loginRateLimit = makeRateLimiter({ limit: 10 });
 const createUserRateLimit = makeRateLimiter({ limit: 5 });
+const refreshTokenRateLimit = makeRateLimiter({ limit: 5 });
 
 userRoutes.post(
   "/users",
@@ -18,4 +20,10 @@ userRoutes.post(
   "/users/login",
   loginRateLimit,
   expressAdapter(makeLoginController()),
+);
+
+userRoutes.post(
+  "/users/refresh-token",
+  refreshTokenRateLimit,
+  expressAdapter(makeRefreshTokenController()),
 );
