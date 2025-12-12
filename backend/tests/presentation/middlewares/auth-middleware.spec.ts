210ebb6ac285eeadf788471b6910e2ea717b0642
maurolps <boroengenharia@gmail.com>
2025-12-11 21:52:47 -0300

    test(auth): ensure AuthMiddleware validates authorization header

---
+++ backend/tests/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest";
+import { AppError } from "@application/errors/app-error";
+import { AuthMiddleware } from "@presentation/middlewares/auth-middleware";
+
+describe("AuthMiddleware", () => {
+  const makeSut = () => {
+    const sut = new AuthMiddleware();
+    return { sut };
+  };
+
+  it("Should throw MISSING_PARAM if authorization header is missing", async () => {
+    const { sut } = makeSut();
+    const dummyRequest = {
+      headers: {},
+    };
+    await expect(sut.handle(dummyRequest)).rejects.toEqual(
+      new AppError(
+        "MISSING_PARAM",
+        "Authorization header is missing or malformed",
+      ),
+    );
+  });
+});
