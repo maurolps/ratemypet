3186bfae38ceb7c7dd70cd0dc8bde79d8427edc5
maurolps <boroengenharia@gmail.com>
2025-12-15 22:48:38 -0300

    test(pet): ensure UploadPetController calls UploadPetUseCase with correct values

---
+++ backend/tests/presentation/doubles/upload-pet.usecase.stub.ts
@@ -0,0 +1,20 @@
+import type { Pet } from "@domain/entities/pet";
+import type {
+  UploadPet,
+  UploadPetDTO,
+} from "@domain/usecases/upload-pet.contract";
+import { FIXED_DATE } from "../../config/constants";
+
+export class UploadPetUseCaseStub implements UploadPet {
+  async execute(petDTO: UploadPetDTO): Promise<Pet> {
+    const pet: Pet = {
+      id: "valid_pet_id",
+      name: petDTO.name,
+      type: "dog",
+      image_url: `valid_pet_image_url`,
+      caption: "generated_caption",
+      created_at: FIXED_DATE,
+    };
+    return pet;
+  }
+}
--- backend/tests/presentation/upload-pet.controller.spec.ts
+++ backend/tests/presentation/upload-pet.controller.spec.ts
@@ -1,13 +1,19 @@
 import { describe, expect, it, vi } from "vitest";
 import { UploadPetController } from "@presentation/controllers/upload-pet.controller";
 import { UploadPetValidatorStub } from "./doubles/upload-pet.validator.stub";
+import { UploadPetUseCaseStub } from "./doubles/upload-pet.usecase.stub";
 
 describe("UploadPetController", () => {
   const makeSut = () => {
     const httpValidatorStub = new UploadPetValidatorStub();
+    const uploadPetUseCaseStub = new UploadPetUseCaseStub();
     const httpValidatorSpy = vi.spyOn(httpValidatorStub, "execute");
-    const sut = new UploadPetController(httpValidatorStub);
-    return { sut, httpValidatorSpy };
+    const uploadPetUseCaseSpy = vi.spyOn(uploadPetUseCaseStub, "execute");
+    const sut = new UploadPetController(
+      httpValidatorStub,
+      uploadPetUseCaseStub,
+    );
+    return { sut, httpValidatorSpy, uploadPetUseCaseSpy };
   };
 
   const dummyRequest = {
@@ -30,5 +36,19 @@ describe("UploadPetController", () => {
     const { sut, httpValidatorSpy } = makeSut();
     await sut.handle(dummyRequest);
     expect(httpValidatorSpy).toHaveBeenCalledWith(dummyRequest);
+  });
+
+  it("Should call UploadPet use case with correct values", async () => {
+    const { sut, uploadPetUseCaseSpy } = makeSut();
+    await sut.handle(dummyRequest);
+    expect(uploadPetUseCaseSpy).toHaveBeenCalledWith({
+      name: "valid_pet_name",
+      userId: "valid_user_id",
+      image: {
+        originalName: "valid_image_name",
+        mimeType: "valid/mime-type",
+        buffer: Buffer.from("any_image_buffer"),
+      },
+    });
   });
 });
