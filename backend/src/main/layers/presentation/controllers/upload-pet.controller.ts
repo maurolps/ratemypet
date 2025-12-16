51547ff4ccc54624e08b7e47a78e8af2e0b03e62
maurolps <boroengenharia@gmail.com>
2025-12-15 22:47:30 -0300

    feat(pet): implement UploadPetUseCase in UploadPetController

--- backend/src/main/layers/presentation/controllers/upload-pet.controller.ts
+++ backend/src/main/layers/presentation/controllers/upload-pet.controller.ts
@@ -1,4 +1,7 @@
-import type { UploadPetDTO } from "@domain/usecases/upload-pet.contract";
+import type {
+  UploadPet,
+  UploadPetDTO,
+} from "@domain/usecases/upload-pet.contract";
 import type { Controller } from "@presentation/contracts/controller.contract";
 import type { HttpValidator } from "@presentation/contracts/http-validator.contract";
 import type { HttpRequest } from "@presentation/dtos/http-request.dto";
@@ -7,12 +10,18 @@ import { ErrorPresenter } from "@presentation/errors/error-presenter";
 import { ok } from "@presentation/http/http-helpers";
 
 export class UploadPetController implements Controller {
-  constructor(private readonly httpValidator: HttpValidator<UploadPetDTO>) {}
+  constructor(
+    private readonly httpValidator: HttpValidator<UploadPetDTO>,
+    private readonly uploadPet: UploadPet,
+  ) {}
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
       const petDTO = this.httpValidator.execute(request);
-      return ok(petDTO);
+
+      const pet = await this.uploadPet.execute(petDTO);
+
+      return ok(pet);
     } catch (error) {
       return ErrorPresenter(error);
     }
