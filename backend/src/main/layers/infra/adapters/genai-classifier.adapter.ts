cc3882f5b17577a6a231461ac25add2c7904cd9e
maurolps <boroengenharia@gmail.com>
2025-12-30 18:20:29 -0300

    feat(pet): add GenAiClassifierAdapter for pet image classification

---
+++ backend/src/main/layers/infra/adapters/genai-classifier.adapter.ts
@@ -0,0 +1,53 @@
+import type { PetClassifier } from "@application/ports/pet-classifier.contract";
+import type { ClassifiedPet } from "@domain/entities/pet";
+import type { UploadPetDTO } from "@domain/usecases/upload-pet.contract";
+import type { GoogleGenAI } from "@google/genai";
+
+export class GenAiClassiferAdapter implements PetClassifier {
+  constructor(private readonly ai: GoogleGenAI) {}
+
+  async classify(petDTO: UploadPetDTO): Promise<ClassifiedPet | null> {
+    const prompt = `
+You are an expert pet image classifier.
+
+Analyze the image and return ONLY a valid JSON object with the following shape:
+
+{
+  "isValidPet": boolean,
+  "petType": "DOG" | "CAT" | null,
+  "caption": string
+}
+
+Rules:
+- isValidPet must be false if the image is not a dog or a cat
+- petType must be null if isValidPet is false
+- caption must be a short, funny sentence related to the pet
+- DO NOT return markdown
+- DO NOT add explanations
+`;
+
+    const imagePart = {
+      inlineData: {
+        data: petDTO.image.buffer.toString("base64"),
+        mimeType: petDTO.image.mimeType,
+      },
+    };
+
+    const response = await this.ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      config: {
+        responseMimeType: "application/json",
+      },
+      contents: [imagePart, { text: prompt }],
+    });
+
+    const aiResponse = JSON.parse(response.text ?? "{}");
+
+    const classifiedPet: ClassifiedPet = {
+      type: aiResponse.petType,
+      caption: aiResponse.caption,
+    };
+
+    return classifiedPet;
+  }
+}
