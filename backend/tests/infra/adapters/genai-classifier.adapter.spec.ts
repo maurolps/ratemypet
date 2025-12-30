d5ce5e47af29b4babc66ca7ec931c8822b803af6
maurolps <boroengenharia@gmail.com>
2025-12-30 18:21:53 -0300

    test(pet): ensure GenAiClassifierAdapter calls ai service only once

---
+++ backend/tests/infra/adapters/genai-classifier.adapter.spec.ts
@@ -0,0 +1,24 @@
+import { vi, it, describe, expect } from "vitest";
+import { googleGenAiStub } from "./doubles/genai.stub";
+
+import { GenAiClassiferAdapter } from "@infra/adapters/genai-classifier.adapter";
+
+describe("GenAiClassifer Adapter", () => {
+  const sut = new GenAiClassiferAdapter(googleGenAiStub);
+  const googleGenAiSpy = vi.spyOn(googleGenAiStub.models, "generateContent");
+
+  const validPetDTO = {
+    petName: "any_pet_name",
+    userId: "any_user_id",
+    image: {
+      originalName: "any_image_name",
+      mimeType: "valid/mime-type",
+      buffer: Buffer.from("any_image_buffer"),
+    },
+  };
+
+  it("Should call GoogleGenAI once", async () => {
+    await sut.classify(validPetDTO);
+    expect(googleGenAiSpy).toHaveBeenCalledTimes(1);
+  });
+});
