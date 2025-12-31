245e3206f88658c3e7b9b1d415044a420551c72e
maurolps <boroengenharia@gmail.com>
2025-12-31 13:41:44 -0300

    feat(pet): add S3PetStorageAdapter for handling pet image uploads and deletions

---
+++ backend/src/main/layers/infra/adapters/s3-pet-storage.adapter.ts
@@ -0,0 +1,37 @@
+import type { PetStorage } from "@application/ports/pet-storage.contract";
+import type { PetImage } from "@domain/entities/pet";
+import {
+  type S3Client,
+  DeleteObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { env } from "@main/config/env";
+
+export class S3PetStorageAdapter implements PetStorage {
+  constructor(private readonly s3Client: S3Client) {}
+
+  async upload(image: PetImage): Promise<string> {
+    const uploadParams = {
+      Bucket: env.AWS_BUCKET_NAME,
+      Key: `${Date.now()}-${image.originalName}`,
+      Body: image.buffer,
+      ContentType: image.mimeType,
+    };
+
+    await this.s3Client.send(new PutObjectCommand(uploadParams));
+
+    const imageUrl = `${env.AWS_ENDPOINT}/${env.AWS_BUCKET_NAME}/${uploadParams.Key}`;
+    return imageUrl;
+  }
+
+  async delete(imageUrl: string): Promise<void> {
+    const url = new URL(imageUrl);
+    const key = url.pathname.replace(`/${env.AWS_BUCKET_NAME}/`, "");
+
+    const deleteParams = {
+      Bucket: env.AWS_BUCKET_NAME,
+      Key: key,
+    };
+    await this.s3Client.send(new DeleteObjectCommand(deleteParams));
+  }
+}
