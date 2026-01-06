301205f343d7b9edd4163598446516aed38b2be1
maurolps <boroengenharia@gmail.com>
2026-01-06 19:26:23 -0300

    test(pet): ensure PgPetRepository save a pet with correct values

---
+++ backend/tests/infra/db/postgres/pg-pet.repository.test.ts
@@ -0,0 +1,25 @@
+import { it, describe, expect } from "vitest";
+import { PgPetRepository } from "@infra/db/postgres/pg-pet.repository";
+import type { UnsavedPet } from "@domain/entities/pet";
+
+describe("PgPetRepository", () => {
+  const unsavedPet: UnsavedPet = {
+    petName: "valid_pet_name",
+    type: "dog",
+    image_url: "https://valid.image.url/pet.png",
+    caption: "Generated caption for the pet",
+  };
+
+  describe("save", () => {
+    it("Should persist and return a Pet on success", async () => {
+      const sut = new PgPetRepository();
+      const pet = await sut.save(unsavedPet);
+      expect(pet.id).toBeTruthy();
+      expect(pet.name).toEqual(unsavedPet.petName);
+      expect(pet.type).toEqual(unsavedPet.type);
+      expect(pet.image_url).toEqual(unsavedPet.image_url);
+      expect(pet.caption).toEqual(unsavedPet.caption);
+      expect(pet.created_at).toBeInstanceOf(Date);
+    });
+  });
+});
