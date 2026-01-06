67829e9090b6390cbeba89b08f41e0d4e09242fc
maurolps <boroengenharia@gmail.com>
2026-01-06 19:25:26 -0300

    feat(pet): implement PgPetRepository for saving pets to the database

---
+++ backend/src/main/layers/infra/db/postgres/pg-pet.repository.ts
@@ -0,0 +1,24 @@
+import type { Pet, UnsavedPet } from "@domain/entities/pet";
+import type { UploadPetRepository } from "@application/repositories/upload-pet.repository";
+import { PgPool } from "./helpers/pg-pool";
+
+export class PgPetRepository implements UploadPetRepository {
+  private readonly pool: PgPool;
+  constructor() {
+    this.pool = PgPool.getInstance();
+  }
+
+  async save(unsavedPet: UnsavedPet): Promise<Pet> {
+    const { petName: name, type, image_url, caption } = unsavedPet;
+    const petRows = await this.pool.query<Pet>(
+      `
+      INSERT INTO pets (name, type, image_url, caption)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `,
+      [name, type, image_url, caption],
+    );
+    const pet = petRows.rows[0];
+    return pet;
+  }
+}
