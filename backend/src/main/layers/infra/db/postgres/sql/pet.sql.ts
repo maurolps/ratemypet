6dee6e952f677ae283a1b703236827d2ca3e3751
maurolps <boroengenharia@gmail.com>
2026-01-10 17:21:45 -0300

    feat(pet): add COUNT_PETS_BY_OWNER_ID query to retrieve pet count by owner

--- backend/src/main/layers/infra/db/postgres/sql/pet.sql.ts
+++ backend/src/main/layers/infra/db/postgres/sql/pet.sql.ts
@@ -4,4 +4,9 @@ export const sql = {
   VALUES ($1, $2, $3, $4, $5)
   RETURNING id, owner_id, name, type, image_url, caption, created_at
   `,
+  COUNT_PETS_BY_OWNER_ID: `
+  SELECT COUNT(*) AS count
+  FROM pets
+  WHERE owner_id = $1
+  `,
 };
