d187576f75bc78e096476d85ff966d11c5cf9945
maurolps <boroengenharia@gmail.com>
2026-01-15 19:57:15 -0300

    test(post): ensure CreatePostUseCase throws if pet does not belong to the author

--- backend/tests/application/create-post.usecase.spec.ts
+++ backend/tests/application/create-post.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { CreatePostUseCase } from "@application/usecases/create-post.usecase";
 import { describe, vi, it, expect } from "vitest";
 import { FindPetRepositoryStub } from "./doubles/find-pet.repository.stub";
+import { AppError } from "@application/errors/app-error";
 
 describe("CreatePostUseCase", () => {
   const makeSut = () => {
@@ -15,7 +16,7 @@ describe("CreatePostUseCase", () => {
 
   const postDTO = {
     pet_id: "valid_pet_id",
-    author_id: "valid_author_id",
+    author_id: "valid_owner_id",
     caption: "valid_caption",
   };
 
@@ -23,5 +24,24 @@ describe("CreatePostUseCase", () => {
     const { sut, findPetRepositorySpy } = makeSut();
     await sut.execute(postDTO);
     expect(findPetRepositorySpy).toHaveBeenCalledWith("valid_pet_id");
+  });
+
+  it("Should return 403 if pet does not belong to the author", async () => {
+    const { sut, findPetRepositorySpy } = makeSut();
+    findPetRepositorySpy.mockResolvedValueOnce({
+      id: "valid_pet_id",
+      owner_id: "different_owner_id",
+      name: "valid_pet_name",
+      type: "dog",
+      image_url: "valid_pet_image_url",
+      caption: "valid_caption",
+      created_at: new Date(),
+    });
+    await expect(sut.execute(postDTO)).rejects.toThrow(
+      new AppError(
+        "FORBIDDEN",
+        "You do not have permission to create a post for this pet.",
+      ),
+    );
   });
 });
