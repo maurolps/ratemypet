db52a326d32a27e718f1b33451b0a06aefd319c2
maurolps <boroengenharia@gmail.com>
2026-01-29 17:10:08 -0300

    test(like): add stubs for LikePostController testing

---
+++ backend/tests/presentation/doubles/like-post.usecase.stub.ts
@@ -0,0 +1,20 @@
+import type {
+  LikePost,
+  LikePostDTO,
+  LikePostResult,
+} from "@domain/usecases/like-post.contract";
+import { FIXED_DATE } from "../../config/constants";
+
+export class LikePostUseCaseStub implements LikePost {
+  async execute(_: LikePostDTO): Promise<LikePostResult> {
+    return {
+      like: {
+        id: "valid_like_id",
+        post_id: "valid_post_id",
+        user_id: "valid_user_id",
+        created_at: FIXED_DATE,
+      },
+      likes_count: 1,
+    };
+  }
+}
