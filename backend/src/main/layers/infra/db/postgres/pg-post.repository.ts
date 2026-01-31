a63cfc70553f73a8ecad8fa8ea438d561a3029dc
maurolps <boroengenharia@gmail.com>
2026-01-31 16:39:41 -0300

    feat(post): implement findById and updateLikesCount methods in PgPostRepository

--- backend/src/main/layers/infra/db/postgres/pg-post.repository.ts
+++ backend/src/main/layers/infra/db/postgres/pg-post.repository.ts
@@ -1,5 +1,7 @@
 import { Post, type PostStatus } from "@domain/entities/post";
 import type { CreatePostRepository } from "@application/repositories/create-post.repository";
+import type { FindPostRepository } from "@application/repositories/find-post.repository";
+import type { UpdateLikesRepository } from "@application/repositories/update-likes.repository";
 import { PgPool } from "./helpers/pg-pool";
 import { sql } from "./sql/post.sql";
 
@@ -14,7 +16,9 @@ type PostRow = {
   comments_count: number;
 };
 
-export class PgPostRepository implements CreatePostRepository {
+export class PgPostRepository
+  implements CreatePostRepository, FindPostRepository, UpdateLikesRepository
+{
   private readonly pool: PgPool;
   constructor() {
     this.pool = PgPool.getInstance();
@@ -30,5 +34,22 @@ export class PgPostRepository implements CreatePostRepository {
     ]);
     const savedPost = postRows.rows[0];
     return Post.rehydrate(savedPost);
+  }
+
+  async findById(postId: string): Promise<Post | null> {
+    const postRows = await this.pool.query<PostRow>(sql.FIND_POST_BY_ID, [
+      postId,
+    ]);
+    const post = postRows.rows[0] || null;
+    return post ? Post.rehydrate(post) : null;
+  }
+
+  async updateLikesCount(post: Post): Promise<Post> {
+    const state = post.toState;
+    const postRows = await this.pool.query<PostRow>(sql.UPDATE_LIKES_COUNT, [
+      state.id,
+    ]);
+    const updatedPost = postRows.rows[0];
+    return Post.rehydrate(updatedPost);
   }
 }
--- backend/src/main/layers/infra/db/postgres/sql/post.sql.ts
+++ backend/src/main/layers/infra/db/postgres/sql/post.sql.ts
@@ -2,6 +2,17 @@ export const sql = {
   CREATE_POST: `
   INSERT INTO posts (pet_id, author_id, caption, status)
   VALUES ($1, $2, $3, $4)
-  RETURNING id, pet_id, author_id, caption, status, created_at
+  RETURNING id, pet_id, author_id, caption, status, created_at, likes_count, comments_count
+  `,
+  FIND_POST_BY_ID: `
+  SELECT id, pet_id, author_id, caption, status, created_at, likes_count, comments_count
+  FROM posts
+  WHERE id = $1
+  `,
+  UPDATE_LIKES_COUNT: `
+  UPDATE posts
+  SET likes_count = likes_count + 1
+  WHERE id = $1
+  RETURNING id, pet_id, author_id, caption, status, created_at, likes_count, comments_count
   `,
 };
