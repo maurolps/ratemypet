40b9fcadb04bc5a4b32c208eff7d24ea6b0d6961
maurolps <boroengenharia@gmail.com>
2026-02-02 23:32:21 -0300

    feat(like): add PgUnitOfWorkAdapter to LikePostUseCase factory

--- backend/src/main/composition/posts/like-post.usecase.factory.ts
+++ backend/src/main/composition/posts/like-post.usecase.factory.ts
@@ -1,14 +1,17 @@
 import { LikePostUseCase } from "@application/usecases/like-post.usecase";
 import { PgLikeRepository } from "@infra/db/postgres/pg-like.repository";
 import { PgPostRepository } from "@infra/db/postgres/pg-post.repository";
+import { PgUnitOfWorkAdapter } from "@infra/db/postgres/adapters/pg-unit-of-work.adapter";
 
 export const makeLikePostUseCase = () => {
   const postRepository = new PgPostRepository();
   const likeRepository = new PgLikeRepository();
+  const unitOfWork = new PgUnitOfWorkAdapter();
   const usecase = new LikePostUseCase(
     postRepository,
     likeRepository,
     postRepository,
+    unitOfWork,
   );
 
   return usecase;
