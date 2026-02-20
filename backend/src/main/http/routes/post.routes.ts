fa455903a2adb645a5c92ad6e8237d55650b7041
maurolps <boroengenharia@gmail.com>
2026-02-20 16:27:24 -0300

    feat(getpost): add get post route

--- backend/src/main/http/routes/post.routes.ts
+++ backend/src/main/http/routes/post.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 import { expressAdapter } from "../adapters/express-controller.adapter";
 import { makeRateLimiter } from "../middlewares/rate-limit";
-import { authMiddleware } from "../middlewares/authenticate";
+import {
+  authMiddleware,
+  optionalAuthMiddleware,
+} from "../middlewares/authenticate";
 import { makeCreatePostController } from "@main/composition/posts/create-post.controller.factory";
 import { makeCreateCommentController } from "@main/composition/posts/create-comment.controller.factory";
+import { makeGetPostController } from "@main/composition/posts/get-post.controller.factory";
 import { makeLikePostController } from "@main/composition/posts/like-post.controller.factory";
 import { makeUnlikePostController } from "@main/composition/posts/unlike-post.controller.factory";
 
 export const postRoutes = Router();
 
 const createPostRateLimit = makeRateLimiter({ limit: 5 });
+const getPostRateLimit = makeRateLimiter({ limit: 30 });
 const createCommentRateLimit = makeRateLimiter({ limit: 10 });
 const likePostRateLimit = makeRateLimiter({ limit: 10 });
 const unlikePostRateLimit = makeRateLimiter({ limit: 10 });
@@ -33,6 +38,13 @@ postRoutes.post(
   authMiddleware(),
   createCommentRateLimit,
   expressAdapter(makeCreateCommentController()),
+);
+
+postRoutes.get(
+  "/posts/:id",
+  optionalAuthMiddleware(),
+  getPostRateLimit,
+  expressAdapter(makeGetPostController()),
 );
 
 postRoutes.delete(
