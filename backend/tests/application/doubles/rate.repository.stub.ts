fe22181b36a04e3419be8f4628d7bfe60cf09a6e
maurolps <boroengenharia@gmail.com>
2026-04-01 19:25:18 -0300

    refactor(tests): ensure RatePetUseCase properly handle upsert result with UoW

--- backend/tests/application/doubles/rate.repository.stub.ts
+++ backend/tests/application/doubles/rate.repository.stub.ts
@@ -1,28 +1,26 @@
-import type { RateRepository } from "@application/repositories/rate.repository";
+import type { Transaction } from "@application/ports/unit-of-work.contract";
+import type {
+  RateRepository,
+  UpsertResult,
+} from "@application/repositories/rate.repository";
 import type { Rate } from "@domain/entities/rate";
 import { FIXED_DATE } from "../../config/constants";
 
 export class RateRepositoryStub implements RateRepository {
-  async findByPetIdAndUserId(
-    petId: string,
-    userId: string,
-  ): Promise<Rate | null> {
-    return {
-      petId,
-      userId,
-      rate: "cute",
-      createdAt: FIXED_DATE,
-      updatedAt: FIXED_DATE,
-    };
+  async findByPetIdAndUserId(_: string, __: string): Promise<Rate | null> {
+    return null;
   }
 
-  async upsert(rate: Rate): Promise<Rate> {
+  async upsert(rate: Rate, _: Transaction = {}): Promise<UpsertResult> {
     return {
-      petId: rate.petId,
-      userId: rate.userId,
-      rate: rate.rate,
-      createdAt: FIXED_DATE,
-      updatedAt: FIXED_DATE,
+      rate: {
+        petId: rate.petId,
+        userId: rate.userId,
+        rate: rate.rate,
+        createdAt: FIXED_DATE,
+        updatedAt: FIXED_DATE,
+      },
+      operation: "created",
     };
   }
 }
