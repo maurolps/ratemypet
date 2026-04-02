eafe926baf1ef6b58779615873d2ef253327215a
maurolps <boroengenharia@gmail.com>
2026-04-01 23:58:43 -0300

    feat(rate): add unit of work to DeleteRateUseCase composition

--- backend/src/main/composition/pets/delete-rate.usecase.factory.ts
+++ backend/src/main/composition/pets/delete-rate.usecase.factory.ts
@@ -1,11 +1,18 @@
 import { DeleteRateUseCase } from "@application/usecases/delete-rate.usecase";
 import { PgPetRepository } from "@infra/db/postgres/pg-pet.repository";
 import { PgRateRepository } from "@infra/db/postgres/pg-rate.repository";
+import { PgUnitOfWorkAdapter } from "@infra/db/postgres/adapters/pg-unit-of-work.adapter";
 
 export const makeDeleteRateUseCase = () => {
   const petRepository = new PgPetRepository();
   const rateRepository = new PgRateRepository();
-  const usecase = new DeleteRateUseCase(petRepository, rateRepository);
+  const unitOfWork = new PgUnitOfWorkAdapter();
+  const usecase = new DeleteRateUseCase(
+    petRepository,
+    rateRepository,
+    petRepository,
+    unitOfWork,
+  );
 
   return usecase;
 };
